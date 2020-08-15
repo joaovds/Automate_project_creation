226635cf7237d481bf6422df116b53dc0769afcf
João Victor <jv782063@gmail.com>
2020-08-15 19:23:16 -0300

    feat: create project type selection menu

--- script.sh
+++ script.sh
@@ -46,11 +46,32 @@ askForName() {
   else
     read -n1 -p "The project path will be this: \"$path\" [y/n]: " confirmation
     case $confirmation in
-      Y | y) echo -e "\r\nconfirm";;
+      Y | y) typeOfProject;;
       N | n) echo -e "\r\nOperation interrupted :-("; exit 0;;
       *) echo -e "\r\nInvalid Operation!"; askForName;;
     esac
   fi
+}
+
+typeOfProject() {
+  echo -e "\r\n"
+  echo -e "\033[01;32m|   What kind of project do you want to create?   |"
+  echo -e "\033[01;32m|-------------------------------------------------|"
+  echo -e "\033[01;32m|   1) NodeJS                                     |"
+  echo -e "\033[01;32m|   2) NodeJS   (TypeScript)                      |"
+  echo -e "\033[01;32m|   3) ReactJS                                    |"
+  echo -e "\033[01;32m|   4) ReactJS  (TypeScript)                      |"
+  echo -e "\033[01;32m|_________________________________________________|"
+
+  read -n1 -p "|--> " project
+
+  case $project in
+    1) echo node;;
+    2) echo node ts;;
+    3) echo react;;
+    4) echo react ts;;
+    *) echo -e "\033[01;31m\r\nOpção Inválida"; typeOfProject;;
+  esac
 }
 
 if [[ "$1"  ]]; then
