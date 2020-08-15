599e2765eb479e5bb6c64612f9ee9d653461542d
João Victor <jv782063@gmail.com>
2020-08-15 18:25:27 -0300

    feat: creates the function that asks for the path where the project will be created, and its initial use next to the menu

--- script.sh
+++ script.sh
@@ -24,3 +24,28 @@ usage() {
 EOF
 }
 
+askForWay() {
+  read -p "Where do you want to create the project? (way after home): " way
+  route=$HOME"/"$way
+
+  if [ -e "$route" ]; then
+    echo All right
+  else
+    echo -e "Invalid path..."
+    askForWay
+  fi
+}
+
+if [[ "$1"  ]]; then
+  while [[ "$1"  ]]; do
+    case "$1" in
+      -h | --help) usage && exit 0;;
+      -v | --version) echo Version 1.0 && exit 0;;
+      *) echo Invalid option && exit 1;
+    esac
+    shift
+  done
+else
+  askForWay
+fi
+
